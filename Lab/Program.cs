346a54d9c69197bc07c43d2c54c189dc7a7a98f1
Festino <offestashka@mail.ru>
2021-10-04 14:14:23 +0300

    Initial commit

---
+++ Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    class Program
+    {
+        // 2b => TPL Flow + progress bar
+
+        static void Main(string[] args)
+        {
+            // input - full path
+            // output - list of files: file name + its objects: class name + bounding box
+            // updating - progress bar + list of classes: class name + count
+            // user can stop recognision
+        }
+    }
+}
