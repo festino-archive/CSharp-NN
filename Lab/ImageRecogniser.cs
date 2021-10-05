795c4ffddd3bee6656f6fb9f185e93359bba782a
Festino <offestashka@mail.ru>
2021-10-05 17:51:41 +0300

    Multithreading v1

--- Lab/ImageRecogniser.cs
+++ Lab/ImageRecogniser.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.Onnx;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -10,6 +12,7 @@ using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using YOLOv4MLNet.DataStructures;
 using static Microsoft.ML.Transforms.Image.ImageResizingEstimator;
+using YoloPredictionEngine = Microsoft.ML.PredictionEngine<YOLOv4MLNet.DataStructures.YoloV4BitmapData, YOLOv4MLNet.DataStructures.YoloV4Prediction>;
 
 namespace Lab
 {
@@ -26,7 +29,7 @@ namespace Lab
             "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
         };
         readonly int ThreadNum;
-        private PredictionEngine<YoloV4BitmapData, YoloV4Prediction>[] PredictionEngines;
+        private ConcurrentStack<YoloPredictionEngine> PredictionEngines;
 
         readonly string ModelPath = "\\YOLOv4 Model\\yolov4.onnx";
         string FullModelPath;
@@ -39,11 +42,11 @@ namespace Lab
         readonly ConsoleProgressBar progress;
         readonly Dictionary<string, int> CurrentResults;
 
-        public ImageRecogniser(string fullPath, int threadNum = 1)
+        public ImageRecogniser(string fullPath, int threadNum = 2)
         {
             ImagePath = fullPath;
             ThreadNum = threadNum;
-            PredictionEngines = new PredictionEngine<YoloV4BitmapData, YoloV4Prediction>[ThreadNum];
+            PredictionEngines = new ConcurrentStack<YoloPredictionEngine>();
             TokenSource = new CancellationTokenSource();
             Token = TokenSource.Token;
             progress = new ConsoleProgressBar();
@@ -67,10 +70,38 @@ namespace Lab
             ParallelOptions options = new ParallelOptions();
             options.CancellationToken = Token;
 
+            var storeInfoBlock = new ActionBlock<RecognisionResult>(recognisionResult =>
+            {
+                result.Add(recognisionResult);
+
+                List<DetectedObject> objects = recognisionResult.Objects;
+                foreach (var obj in objects)
+                {
+                    if (CurrentResults.ContainsKey(obj.Label))
+                        CurrentResults[obj.Label]++;
+                    else
+                        CurrentResults[obj.Label] = 1;
+                }
+                // updating - progress bar + list of classes: class name + count
+                string info = "";
+                foreach (var keyval in CurrentResults)
+                    info = info + keyval.Key + ": " + keyval.Value + "\n";
+                progress.Write(result.Count / (double)filenames.Length, info);
+            },
+            new ExecutionDataflowBlockOptions
+            {
+                CancellationToken = Token,
+                MaxDegreeOfParallelism = 1
+            });
+
             progress.Write(0.0, "");
-            var ab = new ActionBlock<int>(async fileNum => {
+            var processImageBlock = new ActionBlock<int>(fileNum =>
+            {
                 string imageName = filenames[fileNum];
-                var results = Predict(imageName, 0); // TODO switch engine indices
+                YoloPredictionEngine engine;
+                PredictionEngines.TryPop(out engine);
+                var results = Predict(imageName, engine);
+                PredictionEngines.Push(engine);
                 List<DetectedObject> objects = new List<DetectedObject>();
                 foreach (var res in results)
                 {
@@ -85,13 +116,7 @@ namespace Lab
                     else
                         CurrentResults[label] = 1;
                 }
-                result.Add(new RecognisionResult(Path.GetFileName(imageName), objects));
-                options.CancellationToken.ThrowIfCancellationRequested();
-                // updating - progress bar + list of classes: class name + count
-                string info = "";
-                foreach (var keyval in CurrentResults)
-                    info = info + keyval.Key + ": " + keyval.Value + "\n";
-                progress.Write(result.Count / (double)filenames.Length, info);
+                storeInfoBlock.Post(new RecognisionResult(Path.GetFileName(imageName), objects));
             },
             new ExecutionDataflowBlockOptions
             {
@@ -99,10 +124,10 @@ namespace Lab
                 MaxDegreeOfParallelism = ThreadNum
             });
 
-            Parallel.For(0, filenames.Length, options, i => ab.Post(i));
-            ab.Complete();
+            Parallel.For(0, filenames.Length, options, i => processImageBlock.Post(i));
+            processImageBlock.Complete();
 
-            await ab.Completion;
+            await Task.WhenAll(processImageBlock.Completion);
             CurrentResults.Clear();
 
             return result;
@@ -113,21 +138,25 @@ namespace Lab
             TokenSource.Cancel();
         }
 
-        private IReadOnlyList<YoloV4Result> Predict(string fullPath, int index)
+        private IReadOnlyList<YoloV4Result> Predict(string fullPath, YoloPredictionEngine engine)
         {
             using (var bitmap = new Bitmap(Image.FromFile(fullPath)))
             {
-                var predict = PredictionEngines[index].Predict(new YoloV4BitmapData() { Image = bitmap });
+                var predict = engine.Predict(new YoloV4BitmapData() { Image = bitmap });
                 return predict.GetResults(classesNames, 0.3f, 0.7f);
             }
         }
 
         private async Task InitPredictionEngines()
         {
-            var ab = new ActionBlock<int>(async index => {
+            MLContext mlContext = new MLContext();
+            var model = InitModel(mlContext);
+
+            var ab = new ActionBlock<int>(async index =>
+            {
                 //Console.WriteLine("Starting initialization #" + index);
-                var engine = await InitPredictionEngine();
-                PredictionEngines[index] = engine;
+                var engine = mlContext.Model.CreatePredictionEngine<YoloV4BitmapData, YoloV4Prediction>(model);
+                PredictionEngines.Push(engine);
             },
             new ExecutionDataflowBlockOptions
             {
@@ -140,12 +169,11 @@ namespace Lab
             await ab.Completion;
         }
 
-        private Task<PredictionEngine<YoloV4BitmapData, YoloV4Prediction>> InitPredictionEngine()
+
+        private TransformerChain<OnnxTransformer> InitModel(MLContext mlContext)
         {
             // model is available here:
             // https://github.com/onnx/models/tree/master/vision/object_detection_segmentation/yolov4
-
-            MLContext mlContext = new MLContext();
 
             // Define scoring pipeline
             var pipeline = mlContext.Transforms.ResizeImages(inputColumnName: "bitmap", outputColumnName: "input_1:0", imageWidth: 416, imageHeight: 416, resizing: ResizingKind.IsoPad)
@@ -171,11 +199,7 @@ namespace Lab
                     modelFile: FullModelPath, recursionLimit: 100));
 
             // Fit on empty list to obtain input data schema
-            var model = pipeline.Fit(mlContext.Data.LoadFromEnumerable(new List<YoloV4BitmapData>()));
-
-            // Create prediction engine
-            var engine = mlContext.Model.CreatePredictionEngine<YoloV4BitmapData, YoloV4Prediction>(model);
-            return Task.FromResult(engine);
+            return pipeline.Fit(mlContext.Data.LoadFromEnumerable(new List<YoloV4BitmapData>()));
         }
     }
 }
